bb86c9d0af2a9c61cdaed867d9a088ee09325035
BenBasson <github@basson.at>
2014-04-12 17:57:53 +0100

    Anticipate and handle Feedjira parse failures.
    Check that the result from the Feedjira parser is an object that is a
    type of class present in the module Feedjira:Parser.

--- rsscache.rb
+++ rsscache.rb
@@ -1,4 +1,6 @@
 require 'thread'
+require 'feedjira'
+require 'active_support/core_ext/string/inflections'
 
 # Simple class to retrieve, cache and filter results of RSS feeds using Feedjira
 class RSSCache
@@ -34,11 +36,19 @@ class RSSCache
   def update jit_update
     @lock.synchronize do
       if @last_updated.nil? or @last_updated < Time.now - @options[:timeout_secs]
-        puts "#{Time.now.to_formatted_s :db} :: RSSCache Update #{jit_update ? '(JIT) ' : ''}:: #{@url} :: Will cache for #{@options[:timeout_secs]} seconds\n"
-        raw_entries = Feedjira::Feed.fetch_and_parse(@url).entries
-        
-        @entries = @options[:filter].call raw_entries unless raw_entries.size == 0
-        @last_updated = Time.now unless @entries.size == 0
+        puts "#{Time.now.to_formatted_s :db} :: RSSCache Update #{jit_update ? '(JIT) ' : ''}:: #{@url} :: Fetching RSS\n"
+        feed_result = Feedjira::Feed.fetch_and_parse(@url)
+        # Belt and braces check that we've actually been given back a Feedjira Parser class and not a Fixnum 
+        # or perhaps something else entirely, depending on what Feedjira felt like returning (seems to return
+        # HTTP status code as a Fixnum when parsing fails)
+        if not feed_result.nil? and Feedjira::Parser.constants.include? feed_result.class.name.demodulize.to_sym
+          raw_entries = feed_result.entries
+          @entries = @options[:filter].call raw_entries unless raw_entries.size == 0
+          @last_updated = Time.now unless @entries.size == 0
+          puts "#{Time.now.to_formatted_s :db} :: RSSCache Update #{jit_update ? '(JIT) ' : ''}:: #{@url} :: Success - will cache for #{@options[:timeout_secs]} seconds\n"
+        else
+          puts "#{Time.now.to_formatted_s :db} :: RSSCache Update #{jit_update ? '(JIT) ' : ''}:: #{@url} :: Fail - result was #{feed_result.nil? ? 'nil' : feed_result}\n"
+        end
       end
     end
   end
